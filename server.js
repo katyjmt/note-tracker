af27042cd84191374dcf32251fd5e7174eb35e85
katyjmt <43462706+katyjmt@users.noreply.github.com>
2023-08-10 10:59:42 +1000

    Set up server.js file

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const path = require('path');
+const notes = require('./db/db.json');
+const api = require('./routes/index.js');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+// Middleware for parsing JSON & urlencoded data
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// Middleware for serving static files from public folder
+app.use(express.static('public'));
+
+// Send all the requests that begin with /api to the index.js in the routes folder
+app.use('/api', api);
+
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+// GET Route for notes page 
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`)
+});
